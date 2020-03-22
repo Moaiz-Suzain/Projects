48f215012fbd2ced290fc8e41c87062dc5e93397
Moaiz-Suzain <60886000+Moaiz-Suzain@users.noreply.github.com>
2020-03-22 14:10:06 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,26 +1,23 @@
-<?php
+<?php ob_start();
 
 
 
-if(!empty($_POST['submit'])) {
-	$name = $_POST["name"];	
+	if(isset($_POST['submit'])) {
+		
 	$to = "moaizsuzain@gmail.com";
 	$subject = $_POST['subject'];
 	$email = $_POST['email'];
 	$txt = $_POST['message'];
-	$headers = "From: " . $name . "<". $email . ">\r\n":
+	$headers = "From: " .$email . "\r\n" .
 	
-	if(mail($to,$subject,$txt,$headers)) {
-		$message = "Your message has been sent.";
-		$type = "success";
-	}
+	mail($to,$subject,$txt,$headers);
 	
 	header("Location: contact.html");
 	
-    
+
 		
 	
-}
+	}
 
 
 
